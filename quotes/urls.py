3197eab22f83bd1ab53c5aef4e95b1a28e1c3b16
istateside <kevin.r.fleischman@gmail.com>
2015-09-20 16:52:10 -0400

    completed django tutorial

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<quote_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<quote_id>[0-9]+)/author/$', views.quote_author, name='quote_author'),
-    url(r'^(?P<quote_id>[0-9]+)/edit/$', views.edit_quote, name='edit_quote'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/update$', views.update_quote, name='update_quote'),
+    url(r'^new/$', views.new_quote, name="new_quote"),
+    url(r'^create/$', views.create_quote, name="create_quote"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,22 +1,43 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-# from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Quote
 
-
-def index(request):
-    latest_quotes_list = Quote.objects.order_by('-id')[:5]
-    context = { 'quotes': latest_quotes_list }
-    return render(request, 'quotes/index.html', context)
-
-def detail(request, quote_id):
-    quote = get_object_or_404(Quote, pk=quote_id)
-    return render(request, 'quotes/detail.html', { 'quote': quote })
+class IndexView(generic.ListView):
+    template_name = 'quotes/index.html'
+    context_object_name = 'quotes'
     
-def quote_author(request, quote_id):
-    response = "You're looking at the author of quote %s."
-    return HttpResponse(response % quote_id)
+    def get_queryset(self):
+        """Return the last five published quotes"""
+        return Quote.objects.order_by('-id')[:5]
+        
+class DetailView(generic.DetailView):
+    model = Quote
+    template_name = 'quotes/detail.html'
+
+def update_quote(request, pk):
+    quote = get_object_or_404(Quote, pk=pk)
+    try:
+        bodyText = request.POST['body'].strip()
+        if len(bodyText) == 0:
+            raise KeyError
+    except (KeyError):
+        return render(request, 'quotes/detail.html', {
+            'quote': quote,
+            'error_message': "No body? Maybe!"
+        })
+    else:
+        quote.body = bodyText
+        quote.save()
+        return HttpResponseRedirect(reverse('quotes:detail', args=(quote.id,)))
     
-def edit_quote(request, quote_id):
-    return HttpResponse("You're editing quote %s." % quote_id)
+    
+    # return HttpResponse("You're editing quote %s." % quote_id)
+    
+def new_quote(request):
+    return HttpResponse("This will be a form to create new quotes.")
+    
+def create_quote(request, params):
+    return HttpResponse("I guess the create endpoint?")
